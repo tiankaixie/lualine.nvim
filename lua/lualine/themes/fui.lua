528f3420e761fd1fa5f3945bcde2fc7dcf340f47
Tiankai Xie <42656139+tiankaixie@users.noreply.github.com>
2023-10-04 10:35:24 -0700

    bug fix fui

--- lua/lualine/themes/fui.lua
+++ lua/lualine/themes/fui.lua
@@ -15,27 +15,27 @@ local colors = {
 return {
   normal = {
     a = { bg = "#617a81", fg = "#2a3337", gui = 'bold' },
-    b = { bg = "#15191d", fg = "#465864" },
+    b = { bg = "#3c4b4f", fg = "#465864" },
     c = { bg = "#171b1d", fg = "#465864" },
   },
   insert = {
     a = { bg = "#617a81", fg = "#2a3337", gui = 'bold' },
-    b = { bg = "#15191d", fg = "#465864" },
+    b = { bg = "#3c4b4f", fg = "#465864" },
     c = { bg = "#171b1d", fg = "#465864" },
   },
   visual = {
     a = { bg = "#617a81", fg = "#2a3337", gui = 'bold' },
-    b = { bg = "#15191d", fg = "#465864" },
+    b = { bg = "#3c4b4f", fg = "#465864" },
     c = { bg = "#171b1d", fg = "#465864" },
   },
   replace = {
     a = { bg = "#617a81", fg = "#2a3337", gui = 'bold' },
-    b = { bg = "#15191d", fg = "#465864" },
+    b = { bg = "#3c4b4f", fg = "#465864" },
     c = { bg = "#171b1d", fg = "#465864" },
   },
   command = {
     a = { bg = "#617a81", fg = "#2a3337", gui = 'bold' },
-    b = { bg = "#15191d", fg = "#465864" },
+    b = { bg = "#3c4b4f", fg = "#465864" },
     c = { bg = "#171b1d", fg = "#465864" },
   },
   inactive = {
