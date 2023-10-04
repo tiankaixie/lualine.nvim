6b560f79d1d531ea345f4e3c309648dff89cb7f5
Tiankai Xie <42656139+tiankaixie@users.noreply.github.com>
2023-10-04 10:37:49 -0700

    buf gix

--- lua/lualine/themes/fui.lua
+++ lua/lualine/themes/fui.lua
@@ -15,7 +15,7 @@ local colors = {
 return {
   normal = {
     a = { bg = "#617a81", fg = "#2a3337", gui = 'bold' },
-    b = { bg = "#3c4b4f", fg = "#465864" },
+    b = { bg = "#3c4b4f", fg = "#38464e" },
     c = { bg = "#171b1d", fg = "#465864" },
   },
   insert = {
